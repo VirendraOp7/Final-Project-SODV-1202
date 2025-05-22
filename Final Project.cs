338145df0c184c3ece48e5e3e067e4cfaf0ae95a
Vivek Patel <vivekpatel8433@gmail.com>
2025-05-21 23:23:45 -0600

    Add files via upload

---
+++ Final Project.cs
@@ -0,0 +1,10 @@
+﻿namespace Final_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
